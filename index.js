77aa786863343fd92b7722b56cd9ae23cd408dd7
Aditya Kumar <adityakum2004@gmail.com>
2026-02-06 21:50:06 +0530

    added like post

--- index.js
+++ index.js
@@ -1,55 +1,49 @@
-//setup the server
-const express = require("express")  //import express module
-const dotenv = require("dotenv").config() //will allow you to read environment variables from .env file
-const mongoose = require("mongoose") //import mongoose to interact with mongodb
-const userRoutes = require("./routes/user.routes") //import user routes
-const postRoutes = require("./routes/post.routes") //import post routes
-const ProfileRoutes = require("./routes/profile.routes") //import profile routes
-const CommentRoutes = require("./routes/comment.routes") //import comment routes
+// setup the server
+const express = require("express");
+// will allow you to read environment variables from .env file
+const dotenv = require("dotenv").config()
+const mongoose = require("mongoose");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const Profile = require("./models/Profile.model")
-
-//create an express instance
+const cookieParser = require("cookie-parser")
+const userRoutes = require("./routes/user.routes")
+const postRoutes = require("./routes/post.routes")
+const profileRoutes = require("./routes/profile.routes");
+const CommentRoutes = require("./routes/comment.routes")
 const app = express();
 
-//application level middleware
-app.use(cookieParser());
-app.use(express.json()); //to parse json data in request body
+// application-level middleware
+// parse the req.body back to json
+// middleware
 app.use(cors({
-    credentials: true,
-    // origin: "http://localhost:5173",
-    // methods: ["GET", "POST", "PUT", "DELETE"],
-})); //to enable CORS
+    origin: "https://linkedin-frontend-theta.vercel.app",
+    credentials: true
+}));
 
-//connect to mongodb 
-//mongodb -> connection string
+app.use(express.json());
+app.use(cookieParser());
+// mongodb -> connection string
+
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.log("Error connecting to MongoDB: ", err);
-    });
+.then(() => {
+    console.log("Connected to MONGODB")
+}).catch((err) => console.log("err", err.message))
 
-//api
-app.use("/api", userRoutes);
-app.use("/api", postRoutes);
-app.use("/api", ProfileRoutes);
-app.use("/api", CommentRoutes);
+// api
+
+app.use("/api", userRoutes)
+app.use("/api", postRoutes)
+app.use("/api", profileRoutes)
+app.use("/api", CommentRoutes)
 
 app.get("/", (req, res) => {
-    return res.send("<h1>Welcome to LinkedIn Backend!</h1>");
-});
+    return res.send("<h1>Linkedin Backend Project!</h1>")
+})
+// register -> (POST) http://localhost:4000/api/register
 
-//register -> (POST) http://localhost:4000/api/register
-
-//start the server and make it listen to the port specified in .env file
-//backend is now accessible at http://localhost:4000'
+// server-> http://localhost:4000
 
 module.exports = app
 
-
 // app.listen(process.env.PORT, () => {
-//     console.log("Server is running on port ", process.env.PORT); //callback runs once the server starts successfully
+//     console.log("Server is running on port ", process.env.PORT)
 // })
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const { createPost,getAllPosts, likePost } = require('../controllers/post.controler');
 router.post('/createPost', createPost);
 router.get('/get-Posts', getAllPosts);
-router.post('/like', likePost);
+router.post('/likePost', likePost);
 module.exports = router;
