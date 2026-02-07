9d5aecf12a7a199ae3ff23a5f3b1d360b5a61a67
Aditya Kumar <adityakum2004@gmail.com>
2026-02-08 01:55:10 +0530

    Fix: Uncomment app.listen() for local development

--- index.js
+++ index.js
@@ -53,6 +53,8 @@ app.get("/", (req, res) => {
 
 module.exports = app
 
-// app.listen(process.env.PORT, () => {
-//     console.log("Server is running on port ", process.env.PORT)
-// })
+// For local development
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log("Server is running on port ", PORT)
+})
